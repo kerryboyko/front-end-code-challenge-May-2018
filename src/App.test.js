7e809ab06348eab60fc5c179af2484b7842d3907
Brian Boyko <brian.boyko@publicissapient.com>
2019-08-02 13:04:52 +0100

    Github Pages Build

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './App';
-
-describe('App', () => {
-  it('exists', () => {
-    let t = <App fakeProp="foo" />;
-    expect(t.props).toEqual({ fakeProp: 'foo' });
-  });
-});
